49cd52f29e2b32fb8a2a6e5c8fa38bbc59bca79f
yanachuwan9sm <gurem919@icloud.com>
2022-09-28 18:01:22 +0900

    パッケージ分割

--- main.go
+++ main.go
@@ -1,39 +1,21 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/yanachuwan9sm/myapi-tutorial/handlers"
 )
 
 func main() {
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Article...\n")
-	}
-	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article List\n")
-	}
-	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article No.1\n")
-	}
-	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Nice...\n")
-	}
-	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Comment...\n")
-	}
-
 	// 定義した helloHandlerを使うように登録
-	http.HandleFunc("/hello", helloHandler)
-	http.HandleFunc("/article", postArticleHandler)
-	http.HandleFunc("/article/list", articleListHandler)
-	http.HandleFunc("/article/1", articleDetailHandler)
-	http.HandleFunc("/article/nice", postNiceHandler)
-	http.HandleFunc("/comment", postCommentHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	// サーバー起動時のログを出力
 	log.Println("server start at port 8080")
