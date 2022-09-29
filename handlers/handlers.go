5aa7bf23f71ce7091d55e96b547fd6491edf4aab
yanachuwan9sm <gurem919@icloud.com>
2022-09-29 09:29:40 +0900

    gorilla/muxで記述書き換え

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,57 +11,38 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	// 第二引数 req に、受け取った HTTP リクエストの情報が入っている
 
 	// req.Method が GET のときのみ正常応答を返す
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world!\n")
-	} else {
-		// Invalid methodというレスポンスを、405番のステータスコードと共に返す
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	// if req.Method == http.MethodGet {
+	io.WriteString(w, "Hello, world!\n")
+	// } else {
+	// Invalid methodというレスポンスを、405番のステータスコードと共に返す
+	// 	http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	// }
 }
 
 // POST /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Article...\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Article...\n")
 }
 
 // GET /article/list のハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Article List\n")
 }
 
 // GET /article/1 のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		articleID := 1
-		resString := fmt.Sprintf("Article No . %d\n", articleID)
-		io.WriteString(w, resString)
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	articleID := 1
+	resString := fmt.Sprintf("Article No . %d\n", articleID)
+	io.WriteString(w, resString)
 }
 
 // POST /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Nice...\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Nice...\n")
+
 }
 
 // POST /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Article...\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Article...\n")
 }
--- main.go
+++ main.go
@@ -4,21 +4,39 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/yanachuwan9sm/myapi-tutorial/handlers"
 )
 
 func main() {
 
+	//? 標準パッケージ　net/http のみでルーティングを実装
+
 	// 定義した helloHandlerを使うように登録
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	// http.HandleFunc("/hello", handlers.HelloHandler)
+	// http.HandleFunc("/article", handlers.PostArticleHandler)
+	// http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	// http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	// http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	// http.HandleFunc("/comment", handlers.PostCommentHandler)
+
+	//? gorilla/mux(サードパーティー) パッケージでルーティングを実装
+
+	// ルータの作成
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
 	// サーバー起動時のログを出力
 	log.Println("server start at port 8080")
 	//  ListenAndServe関数にて、サーバーを起動
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	//* 第二引数は、サーバーの中で使うルータを指定するもの。標準パッケージ net/http では
+	//* ルータが渡されず nil だったのは、
+	//* Go の HTTP サーバーがデフォルトで持っているルータが自動的に採用されているため。
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
