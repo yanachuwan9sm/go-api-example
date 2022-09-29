04eedd33cd521c42d5dffbed184fd06d317f7165
[メインのGitHubアカウント名] <[GitHub]>
2022-09-29 12:28:16 +0900

    リクエストボディ取得・jsonデコード処理追加

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -19,19 +20,54 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 // POST /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
-	article := models.Article1
+	//? リクエストボディの内容を格納するバイト列を作成
+
+	// リクエストヘッダの Content-Length フィールドの値を取得
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+
+	if err != nil {
+		// getメソッドの返却値(string)からint型への変換が失敗した場合 400 番エラー (BadRequest) を返却
+		http.Error(w, "cannnot get content length\n", http.StatusBadRequest)
+		return
+	}
+	// make関数を使ってその長さのバイトスライスを作成
+	reqBodybuffer := make([]byte, length)
+
+	//? Readメソッドでリクエストボディを読み出し
+
+	//* _, err := req.Body.Read(reqBodybuffer);
+	// 変数 reqBodybuffer にリクエストボディの内容が入る
+	// 戻り値 err に、読み取り時に起きたエラーの内容が格納される
+
+	//* Read メソッドから返ってきたエラーの種類によって、正常にボディの読み込みが終わったのかそうでないのかが分かれます。
+	// 戻り値で返ってきたエラーが io.EOF かどうかを判定するために、標準パッケージ errors の中にある errors.Is 関数を利用
+	// エラーが io.EOF だった場合: 正常にボディの中身を最後まで読み取ることができた
+	// エラーが io.EOF 以外だった場合: ボディの中身を読み取る際に異常が発生した
+
+	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
+		// エラーが io.EOF以外の場合、サーバー内で異常が起きたことを示すエラー(500)を返却
+		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
+		return
+	}
+
+	// ボディを Close する
+	defer req.Body.Close()
+
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+		http.Error(w, "fail to docode json\n", http.StatusBadRequest)
+		return
+	}
+
+	// デコードした構造体を再度エンコード
+	article := reqArticle
 	jsonData, err := json.Marshal(article)
 	if err != nil {
 		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
 		return
 	}
-
-	// json.Marshal 関数から得られた json エンコードの結果は string 型ではなく []byte 型
-	// io.WriteString関数はstring型を引数とするため、[]byte型をstring型に変換する必要がある
-
-	// []byte型であるjsonDataを
-	// string型に変換することなくそのままレスポンス書き込みに使うことができる
 	w.Write(jsonData)
+
 }
 
 // GET /article/list のハンドラ
