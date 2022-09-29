b3d8c8bbaa07231fa53b48b9c31b2d0e1dcc7121
[メインのGitHubアカウント名] <[GitHub]>
2022-09-29 09:12:42 +0900

    許容HTTPメソッドを絞る

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,34 +6,62 @@ import (
 	"net/http"
 )
 
-// /hello のハンドラ
+// GET /hello のハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+	// 第二引数 req に、受け取った HTTP リクエストの情報が入っている
+
+	// req.Method が GET のときのみ正常応答を返す
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello, world!\n")
+	} else {
+		// Invalid methodというレスポンスを、405番のステータスコードと共に返す
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
-// /article のハンドラ
+// POST /article のハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
-// /article/list のハンドラ
+// GET /article/list のハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
-// /article/1 のハンドラ
+// GET /article/1 のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
-	resString := fmt.Sprintf("Article No . %d\n", articleID)
-	io.WriteString(w, resString)
+	if req.Method == http.MethodGet {
+		articleID := 1
+		resString := fmt.Sprintf("Article No . %d\n", articleID)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
-// /article/nice のハンドラ
+// POST /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
-// /comment のハンドラ
+// POST /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
