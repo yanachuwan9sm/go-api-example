2ba0270ecb7fe9841799b0de3bfee179b32aa849
yanachuwan9sm <gurem919@icloud.com>
2022-09-29 10:49:59 +0900

    クエリパラメータを取得

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -29,13 +29,37 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 // GET /article/list のハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+
+	queryMap := req.URL.Query()
+
+	var page int
+
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+
+		// パラメータ page に対応する 1 つ目の値を採用し、それを数値に変換する
+		var err error
+		page, err = strconv.Atoi(p[0]) // パラメータ page に対応する 1 つ目の値を採用し、それを数値に変換する
+
+		if err != nil {
+			// 数字に変換できなかった場合には、リクエスト不正(400)のエラーを返す
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+
+	} else {
+		// クエリパラメータがURLに存在しない場合
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 // GET /article/{id} のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
+		// パスパラメータが不明な場合、リクエスト不正のエラー(400)を返却
 		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 		return
 	}
