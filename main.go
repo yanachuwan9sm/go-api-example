c9da5355a060c3617de0a534fba636801255bd5a
yanachuwan9sm <gurem919@icloud.com>
2022-10-04 10:08:50 +0900

    Query Exec Scan メソッドを用いたDB操作

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/yanachuwan9sm/myapi-tutorial/handlers"
 )
 
 func main() {
+
+	// 接続に使うユーザー・パスワード・データベース名を定義
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+
+	// データベースに接続するためのアドレス文を定義
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	// データベースに接続
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	// sql.DB型のPingメソッドで疎通確認をする
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
 
 	//? 標準パッケージ　net/http のみでルーティングを実装
 
@@ -22,8 +47,7 @@ func main() {
 
 	//? gorilla/mux(サードパーティー) パッケージでルーティングを実装
 
-	// ルータの作成
-	r := mux.NewRouter()
+	r := mux.NewRouter() // ルータの作成
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
 	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
@@ -33,6 +57,7 @@ func main() {
 
 	// サーバー起動時のログを出力
 	log.Println("server start at port 8080")
+
 	//  ListenAndServe関数にて、サーバーを起動
 	//* 第二引数は、サーバーの中で使うルータを指定するもの。標準パッケージ net/http では
 	//* ルータが渡されず nil だったのは、
---
+++ repositories/articles.go
@@ -0,0 +1,116 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/yanachuwan9sm/myapi-tutorial/models"
+)
+
+const (
+	articleNumPerPage = 5
+)
+
+// 新規投稿をデータベースに insert する関数
+// -> データベースに保存した記事内容と、発生したエラーを返り値にする
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+
+	const sqlStr = `
+	insert into articles (title, contents, username, nice, created_at) values (?, ?, ?, 0, now());
+	`
+
+	var newArticle models.Article
+	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	id, _ := result.LastInsertId()
+
+	newArticle.ID = int(id)
+
+	return newArticle, nil
+}
+
+// 変数 page で指定されたページに表示する投稿一覧をデータベースから取得する関数
+// -> 取得した記事データと、発生したエラーを返り値にする
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+
+	const sqlStr = `
+	select article_id, title, contents, username, nice
+	from articles
+	limit ? offset ?;
+	`
+	// 指定された記事データをデータベースから取得
+	rows, err := db.Query(sqlStr, articleNumPerPage, ((page - 1) * articleNumPerPage))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// rowsからデータを取り出し models.Article構造体に格納
+
+	//models.Article型のスライスarticleArrayを生成
+	articleArray := make([]models.Article, 0)
+
+	// 取り出したデータを models.Article 構造体のスライス []models.Article に詰めて返す処理
+	for rows.Next() {
+
+		var article models.Article
+
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		articleArray = append(articleArray, article)
+
+	}
+
+	return articleArray, nil
+}
+
+// いいねの数を update する関数
+// -> 発生したエラーを返り値にする
+func UpdateNiceNum(db *sql.DB, articleID int) error {
+
+	// 指定された ID の記事のいいね数を+1 するようにデータベースの中身を更新する処理
+
+	// トランザクションの開始
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	// 現在のいいね数を取得するクエリの実行し変数 nicenum に取得した値を読み込む
+
+	const sqlGetNice = ` select nice
+			from articles
+			where article_id = ?;
+		`
+	row := tx.QueryRow(sqlGetNice, articleID)
+	if err := row.Err(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	var nicenum int
+	err = row.Scan(nicenum)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// いいね数を +1 する処理
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?`
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, articleID)
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
